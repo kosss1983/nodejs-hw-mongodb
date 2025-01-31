7d43aaea1900a7fd62b7654705c6726ff18f04f0
constantine1983 <constantineck83@gmail.com>
2025-01-31 04:54:25 +0200

    completed for checking

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import pino from "pino-http";
 import cors from "cors";
 
-import { getAllContacts, getContactById } from "./services/contacts.js";
+import contactsRouter from "./routers/contacts.js";
 import { getEnvVar } from "./utils/getEnvVar.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 // Define server port
 const PORT = Number(getEnvVar("PORT", "3000"));
@@ -24,39 +26,9 @@ export const setupServer = () => {
     });
   });
 
-  app.get("/contacts", async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: contacts,
-    });
-  });
-
-  app.get("/contacts/:contactId", async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: "Contact not found",
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contact with id {contactId}!",
-      data: contact,
-    });
-  });
-
-  app.use("*", (req, res) => {
-    res.status(404).json({
-      message: "Not found!",
-    });
-  });
+  app.use(contactsRouter);
+  app.use("*", notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,3 +7,26 @@ export const getAllContacts = async () => {
 export const getContactById = async (contactId) => {
   return await contactsCollection.findById(contactId);
 };
+
+export const createContact = async (payload) => {
+  return await contactsCollection.create(payload);
+};
+
+export const deleteContact = async (contactId) => {
+  return await contactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const result = await contactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      ...options,
+    }
+  );
+
+  return result;
+};
