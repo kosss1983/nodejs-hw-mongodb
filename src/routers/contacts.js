07c2af55afaf88c2e3de46e0f98ca818a6a2d36f
constantine1983 <constantineck83@gmail.com>
2025-02-08 00:04:47 +0200

    completed for checking

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,13 +7,25 @@ import {
   patchContactController,
   deleteContactController,
 } from "../controllers/contacts.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { createContactValidationSchema } from "../validation/contacts.js";
+import { isValidId } from "../middlewares/isValidId.js";
 
 const router = Router();
 
 router.get("/", ctrlWrapper(getAllContactsController));
-router.get("/:contactId", ctrlWrapper(getContactByIdController));
-router.post("/", ctrlWrapper(createContactController));
-router.patch("/:contactId", ctrlWrapper(patchContactController));
-router.delete("/:contactId", ctrlWrapper(deleteContactController));
+router.get("/:contactId", isValidId, ctrlWrapper(getContactByIdController));
+router.post(
+  "/",
+  validateBody(createContactValidationSchema),
+  ctrlWrapper(createContactController)
+);
+router.patch(
+  "/:contactId",
+  isValidId,
+  validateBody(createContactValidationSchema),
+  ctrlWrapper(patchContactController)
+);
+router.delete("/:contactId", isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
